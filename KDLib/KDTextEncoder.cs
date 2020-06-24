2710aa84935e414617bede823c02efb00bb77cf6
David0403 (laptop) <48942146+Noboroto@users.noreply.github.com>
2020-06-24 14:39:39 +0700

    Cretate KDLib
    Add TextEncoder

---
+++ KDLib/KDTextEncoder.cs
@@ -0,0 +1,25 @@
+﻿namespace KDLib
+{
+    public class KDTextEncoder
+    {
+		public static string GetCode(string rawdata)
+		{
+			string text = "";
+			foreach (char c in rawdata)
+			{
+				text += ((char)(ushort)(c + 1372)).ToString();
+			}
+			return text;
+		}
+
+		public static string GetString(string rawdata)
+		{
+			string text = "";
+			foreach (char c in rawdata)
+			{
+				text += ((char)(ushort)(c - 1372)).ToString();
+			}
+			return text;
+		}
+	}
+}
