ad2b528c91c8aea5581a8d14b436e73e87d9e19e
David0403 <48942146+Noboroto@users.noreply.github.com>
2020-07-05 22:16:03 +0700

    Update

---
+++ KDLib/KDObjBase.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace KDLib
+{
+	public abstract class KDObjBase : INotifyPropertyChanged
+	{
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		internal void OnPropertyChanged(string name)
+		{
+			if (this.PropertyChanged != null)
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+			}
+		}
+	}
+}
---
+++ KDLib/Question.cs
@@ -0,0 +1,51 @@
+﻿namespace KDLib
+{
+	public abstract class Question : KDObjBase
+	{
+		private string _Content;
+
+		private string _Answer;
+
+		public string Content
+		{
+			get
+			{
+				return _Content;
+			}
+			set
+			{
+				_Content = value;
+				OnPropertyChanged("Content");
+			}
+		}
+
+		public string Answer
+		{
+			get
+			{
+				return _Answer;
+			}
+			set
+			{
+				_Answer = value;
+				OnPropertyChanged("Answer");
+			}
+		}
+
+		internal Question(string content, string answer)
+		{
+			Content = content;
+			Answer = answer;
+		}
+
+		public static int ContentComparer(Question a, Question b)
+		{
+			return string.Compare(a.Content, b.Content);
+		}
+
+		public static int AnswerComparer(Question a, Question b)
+		{
+			return string.Compare(a.Answer, b.Answer);
+		}
+	}
+}
