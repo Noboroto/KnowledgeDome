fe26568502f8175ef5591bbca3b16deb7b7966ab
David0403 (laptop) <48942146+Noboroto@users.noreply.github.com>
2020-07-13 15:53:18 +0700

    Update Data.cs

--- KDLib/Data.cs
+++ KDLib/Data.cs
@@ -55,68 +55,7 @@ namespace KDLib
 		/// </summary>
 		public static void Initialize()
 		{
-			/*
-			if (!Directory.Exists(Directory.GetCurrentDirectory() + "\\Tests\\Images"))
-			{
-				Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\Tests\\Images");
-			}
-			if (!Directory.Exists(Directory.GetCurrentDirectory() + "\\Tests\\Clips"))
-			{
-				Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\Tests\\Clips");
-			}
-			if (!Directory.Exists(Directory.GetCurrentDirectory() + "\\Tests\\Sounds"))
-			{
-				Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\Tests\\Sounds");
-			}
-			if (File.Exists("Matches.etai"))
-			{
-				Matches = new MatchList(new DataProvider(AIEncoder.GetString(File.ReadAllText("Matches.etai", Encoding.UTF8)), haskey: false));
-			}
-			else
-			{
-				Matches = new MatchList();
-			}
-			if (File.Exists("Tests\\Start.etai"))
-			{
-				StartQuestions = new StartQuestionList(new DataProvider(AIEncoder.GetString(File.ReadAllText("Tests\\Start.etai", Encoding.UTF8)), haskey: false));
-			}
-			else
-			{
-				StartQuestions = new StartQuestionList();
-			}
-			if (File.Exists("Tests\\Obstacle.etai"))
-			{
-				Obstacles = new ObstacleList(new DataProvider(AIEncoder.GetString(File.ReadAllText("Tests\\Obstacle.etai", Encoding.UTF8)), haskey: false));
-			}
-			else
-			{
-				Obstacles = new ObstacleList();
-			}
-			if (File.Exists("Tests\\Acceleration.etai"))
-			{
-				AccelerationQuestions = new AccelerationQuestionList(new DataProvider(AIEncoder.GetString(File.ReadAllText("Tests\\Acceleration.etai", Encoding.UTF8)), haskey: false));
-			}
-			else
-			{
-				AccelerationQuestions = new AccelerationQuestionList();
-			}
-			if (File.Exists("Tests\\Finish.etai"))
-			{
-				FinishQuestions = new FinishQuestionList(new DataProvider(AIEncoder.GetString(File.ReadAllText("Tests\\Finish.etai", Encoding.UTF8)), haskey: false));
-			}
-			else
-			{
-				FinishQuestions = new FinishQuestionList();
-			}
-			if (File.Exists("Tests\\Extra.etai"))
-			{
-				ExtraQuestions = new ExtraQuestionList(new DataProvider(AIEncoder.GetString(File.ReadAllText("Tests\\Extra.etai", Encoding.UTF8)), haskey: false));
-			}
-			else
-			{
-				ExtraQuestions = new ExtraQuestionList();
-			}
-			*/
+
 		}
 
         /// <summary>
