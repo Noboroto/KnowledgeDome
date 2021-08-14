35fc92c78d9bf11f291c405f69fb220b7fc9e71b
David0403 <48942146+Noboroto@users.noreply.github.com>
2021-08-14 13:46:58 +0700

    Edit main oage view

--- KDCtrlLib/Views/MainClientPage.xaml.cs
+++ KDCtrlLib/Views/MainClientPage.xaml.cs
@@ -14,9 +14,11 @@ namespace KDCtrlLib.Views
 		public MainClientPage()
 		{
 			InitializeComponent();
+			var b = new Player(12, "", "adsasd asdasdasdas");
 			var show = new List<PlayerView> { first, second, third, fourth, fifth };
-			for (int i = 0; i < show.Count; ++i)
+			for (int i = 0; i < show.Count - 1; ++i)
 			{
+				show[i].PlayerData = b;
 				show[i].Visibility = System.Windows.Visibility.Visible;
 			}
 		}
