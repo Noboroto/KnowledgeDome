3962fe67dea92499cfba80db715c31cead2707e0
David0403 <48942146+Noboroto@users.noreply.github.com>
2021-08-18 10:28:56 +0700

    New PlayerMainServerView

---
+++ KDCtrlLib/KDControl/PlayerMainServerView.xaml.cs
@@ -0,0 +1,53 @@
+﻿using KDLib;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KDCtrlLib.KDControl
+{
+	/// <summary>
+	/// Interaction logic for PlayerMainServerView.xaml
+	/// </summary>
+	public partial class PlayerMainServerView : UserControl
+	{
+		public static readonly DependencyProperty SizeProperty =
+			DependencyProperty.Register("Size", typeof(int),
+										typeof(PlayerMainServerView)
+										);
+		public int Size
+		{
+			get => (int)GetValue(SizeProperty);
+			set => SetValue(SizeProperty, value);
+		}
+		private Player _PlayerData;
+
+		public Player PlayerData
+		{
+			get => _PlayerData;
+			set
+			{
+				_PlayerData = value;
+				DataContext = _PlayerData;
+			}
+		}
+		public ImageSource Source => _PlayerData.Avatar;
+
+		public int RectangleSize => Size + 10;
+		public PlayerMainServerView()
+		{
+			InitializeComponent();
+		}
+	}
+}
