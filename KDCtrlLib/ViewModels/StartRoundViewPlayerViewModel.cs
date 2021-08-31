2c75df94c62b33476481677bb3a7db8ff45a92c7
David0403 <48942146+Noboroto@users.noreply.github.com>
2021-08-31 21:12:39 +0700

    Add properies

--- KDCtrlLib/ViewModels/StartRoundViewPlayerViewModel.cs
+++ KDCtrlLib/ViewModels/StartRoundViewPlayerViewModel.cs
@@ -9,7 +9,8 @@ namespace KDCtrlLib.ViewModels
 	{
 		private Player _CurrentPlayer;
 		private StartQuestion _CurrentQuestion;
-		private int QuestCount;
+		private int _QuestCount;
+		private double _Timer;
 
 		public Player CurrentPlayer
 		{
@@ -21,16 +22,33 @@ namespace KDCtrlLib.ViewModels
 			get => _CurrentQuestion;
 			set => Set(ref _CurrentQuestion, value);
 		}
+		public int QuestCount
+		{
+			get => _QuestCount;
+			set => Set(ref _QuestCount, value);
+		}
+		public double Timer
+		{
+			get => _Timer;
+			set => Set(ref _Timer, value);
+		}
 
 
 		public StartRoundViewPlayerViewModel()
 		{
-			var b = new Player(12, "asdasd")
+			DebugMethod();
+		}
+
+		private void DebugMethod ()
+		{
+			CurrentPlayer = new Player(12, "asdasd")
 			{
 				BackgroundColor = (Brush)new BrushConverter().ConvertFromString(@"#16acea"),
 				ForegroundColor = Brushes.Black
 			};
-			CurrentPlayer = b;
+			CurrentQuestion = new StartQuestion("Toán học", "abc", "ad", 0);
+			QuestCount = 2;
+			Timer = 23.5;
 		}
 	}
 }
