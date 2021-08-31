867bf718f222f8fa85446301acd68a17d880614d
David0403 <48942146+Noboroto@users.noreply.github.com>
2021-08-31 18:57:56 +0700

    remove

--- DemoWPF/MainWindow.xaml.cs
+++ DemoWPF/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace DemoWPF
         {
             InitializeComponent();
             Data.ClientInitialize();
-            MyFrame.Navigate(new StartRoundPlayerPage());
+            //MyFrame.Navigate(new StartRoundPlayerPage());
         }
     }
 }
