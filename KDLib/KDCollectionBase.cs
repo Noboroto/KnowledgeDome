50fe1227ca48be28e80d2c54c70dcd64cb0b683e
David0403 <48942146+Noboroto@users.noreply.github.com>
2021-09-03 14:10:04 +0700

    Revert "Creatory" (#6)

--- KDLib/KDCollectionBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.ObjectModel;
-
-namespace KDLib
-{
-    public abstract class KDCollectionBase<T> : ObservableCollection<T>
-    {
-        public abstract bool Contains(object id);
-    }
-}
