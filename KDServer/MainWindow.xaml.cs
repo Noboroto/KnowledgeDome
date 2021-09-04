6ee12c3c1037b7161ba7f558749d671a0df28d9e
David0403 <48942146+Noboroto@users.noreply.github.com>
2021-09-04 22:34:58 +0700

    Create KnowledgeDomeServer

---
+++ KDServer/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using KDCtrlLib.Views.ServerView;
+
+using System.Windows;
+
+namespace KDServer
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
