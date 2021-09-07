c8aee0d1b1c1dffbb6d010a97df89a8e2481d72b
David0403 <48942146+Noboroto@users.noreply.github.com>
2021-09-07 10:52:44 +0700

    Add Client App

---
+++ KDClient/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+using KDLib;
+
+namespace KDClient
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			Data.ClientInitialize();
+		}
+	}
+}
